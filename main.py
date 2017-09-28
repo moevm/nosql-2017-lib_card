5fc8358052840e4720681efd2dc95ebb2a824aee
Vladimir Turov <aaaaaa2493@yandex.ru>
2017-09-28 15:01:34 +0300

    added connection through the internet for all DBs

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-import memcache, neo4j.v1
-import memcache, pymongo
-mc = memcache.Client(['188.134.82.95:7005'], debug=0)
-mc.set("default_message", "Hello World!")
+import neo4j.v1
+from pymongo import MongoClient
+from memcache import Client as MemcacheClient
+
+ip = '188.134.82.95'
+mc_port = 7005
+neo4j_port = 7007
+mongo_port = 7009
+
+# memcached test
+
+mc = MemcacheClient([f'{ip}:{mc_port}'], debug=0)
 print(mc.get("default_message"))
 
 
-#neo4j test
+# neo4j test
 
-driver = neo4j.v1.GraphDatabase.driver("bolt://localhost:7687", auth=neo4j.v1.basic_auth("neo4j", "neo4jdefpass"))
+driver = neo4j.v1.GraphDatabase.driver(f"bolt://{ip}:{neo4j_port}", auth=neo4j.v1.basic_auth("neo4j", "pineo4j"))
 session = driver.session()
-
-session.run("CREATE (a:Phrase {start: {start}, end: {end}})",
-          {"start": "Hello, World!", "end": " by neo4j"})
 
 result = session.run("MATCH (a:Phrase) WHERE a.start = {start} "
                    "RETURN a.start AS start, a.end AS end",
@@ -20,13 +25,10 @@ for record in result:
     print("%s %s" % (record["start"], record["end"]))
 
 session.close()
+
 # Pymongo hello world
 
-client = pymongo.MongoClient("localhost", 27017)
+client = MongoClient(ip, mongo_port)
 db = client.test
-print(db.name)
-print(db.my_collection.insert_one({"x": 10}).inserted_id)
-print(db.my_collection.insert_one({"x": 8}).inserted_id)
-print(db.my_collection.insert_one({"x": 11}).inserted_id)
 for item in db.my_collection.find():
-    print(item["x"])
+    print(item["hello world"])
