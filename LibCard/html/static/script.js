82d4a1376eb288be970834d3af7234d62e55c01b
Igor Shadrintcev <ids017@protonmail.com>
2017-11-29 22:26:20 +0300

    frontend part of card updating implemented

--- LibCard/html/static/script.js
+++ LibCard/html/static/script.js
@@ -13,8 +13,11 @@ window.onload = function() {
     settingsIcon = document.getElementById("settings-icon");
     addSidebar.style = "display: none"
     settingsSidebar.style = "display: none"
+    loadAllCards();
+}
 
-    //load all cards
+function loadAllCards() {
+
     var json = JSON.stringify({
         action: "get-all"
     });
@@ -46,8 +49,6 @@ window.onload = function() {
         };
     }
     xhr.send(json);
-
-
 }
 
 function switchSidebar(sidebar) {
@@ -204,12 +205,13 @@ function openUpdateForm(id) {
             console.log(response);
             document.getElementById("update-form-id").innerHTML = response.id;
             document.getElementById("update-form-state").innerHTML = available;
-            document.getElementById("update-form-title").innerHTML = response.title;
-            document.getElementById("update-form-author").innerHTML = response.author;
-            document.getElementById("update-form-year").innerHTML = response.year;
+            document.getElementById("update-form-title").value = response.title;
+            document.getElementById("update-form-author").value = response.author;
+            document.getElementById("update-form-year").value = response.year;
             document.getElementById("date-label").innerHTML = dateLabel;
             document.getElementById("give-take").innerHTML = giveTake;
             document.getElementById("history").innerHTML = historyToHTML(response.history);
+            document.getElementById("update-form-image").src = response.image;
             document.getElementById("overlay").style = "display: block";
         }
     }
@@ -222,4 +224,39 @@ function historyToHTML(history) {
         html += history[i].reader + ", " + history[i].from + " - " + history[i].to + "<br>";
     }
     return html;
+}
+
+function update() {
+    var json = JSON.stringify({
+        action: "update",
+        id: document.getElementById("update-form-id").innerHTML + "",
+        title: document.getElementById("update-form-title").value,
+        author: document.getElementById("update-form-author").value,
+        year: document.getElementById("update-form-year").value
+    });
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", '/', true)
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhr.onreadystatechange = function() {
+        if(this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            if (JSON.parse(xhr.responseText).success == true) {
+                document.getElementById("update-result").innerHTML = "Карточка успешно изменена";
+                closeUpdateForm();
+                loadAllCards();
+            } else {
+                document.getElementById("update-result").innerHTML = "Ошибка при изменении карточки";
+            }
+        } else {
+            document.getElementById("update-result").innerHTML = "Ошибка подключения";
+        };
+    }
+    xhr.send(json);
+}
+
+function remove() {
+    //TODO:remove card
+}
+
+function giveTake() {
+    //TODO:give or take card
 }
