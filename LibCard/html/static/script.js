e736d5f5025acc65d3ea971037f92aa2a8a62a3a
Igor Shadrintcev <ids017@protonmail.com>
2017-11-29 23:07:13 +0300

    give/return book implemented

--- LibCard/html/static/script.js
+++ LibCard/html/static/script.js
@@ -181,50 +181,11 @@ function closeUpdateForm() {
     document.getElementById("overlay").style = "display: none";
 }
 
-function openUpdateForm(id) {
-
-    var json = JSON.stringify({
-        action: "get-info",
-        id: id + ""
-    });
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", '/', true)
-    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    xhr.onreadystatechange = function() {
-        if(this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-
-            var response = JSON.parse(xhr.responseText);
-
-            var available = "Книга занята", dateLabel = "Дата возврата", giveTake = "Принять книгу";
-            document.getElementById("reader-block").style = "display: none";
-            if (response.available) {
-                available = "Кнгига свободна";
-                dateLabel = "Дата выдачи";
-                giveTake = "Выдать книгу";
-                document.getElementById("reader-block").style = "display: block";
-            }
-
-            console.log(response);
-            document.getElementById("update-result").innerHTML = "";
-            document.getElementById("update-form-id").innerHTML = response.id;
-            document.getElementById("update-form-state").innerHTML = available;
-            document.getElementById("update-form-title").value = response.title;
-            document.getElementById("update-form-author").value = response.author;
-            document.getElementById("update-form-year").value = response.year;
-            document.getElementById("date-label").innerHTML = dateLabel;
-            document.getElementById("give-take").innerHTML = giveTake;
-            document.getElementById("history").innerHTML = historyToHTML(response.history);
-            document.getElementById("update-form-image").src = response.image;
-            document.getElementById("overlay").style = "display: block";
-        }
-    }
-    xhr.send(json);
-}
-
 function historyToHTML(history) {
     var html = "";
     for (var i = 0; i < history.length; i++) {
-        html += history[i].reader + ", " + history[i].from + " - " + history[i].to + "<br>";
+        html += history[i].reader + ", " + history[i].from ;
+        if (history[i].to != "") html += " - " + history[i].to + "<br>";
     }
     return html;
 }
@@ -279,6 +240,72 @@ function remove() {
 }
 
 function giveTake() {
-    if (document.getElementById("update-form-state").innerHTML == "Кнгига свободна") take()
-    else give();
+    var actionType, cardId = document.getElementById("update-form-id").innerHTML + "";
+    if (document.getElementById("update-form-state").innerHTML == "Кнгига свободна") actionType = "give-card"
+    else actionType = "return-card";
+    var json = JSON.stringify({
+        action: actionType,
+        id: cardId,
+        reader: document.getElementById("reader").value,
+        date: document.getElementById("date").value + ""
+    });
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", '/', true)
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhr.onreadystatechange = function() {
+        if(this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            if (JSON.parse(xhr.responseText).success == true) {
+                openUpdateForm(cardId);
+                document.getElementById("update-result").innerHTML = "История обновлена";
+            } else {
+                document.getElementById("update-result").innerHTML = "Ошибка при изменении истории";
+            }
+        } else {
+            document.getElementById("update-result").innerHTML = "Ошибка подключения";
+        };
+    }
+    xhr.send(json);
+}
+
+function openUpdateForm(id) {
+
+    var json = JSON.stringify({
+        action: "get-info",
+        id: id + ""
+    });
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", '/', true)
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhr.onreadystatechange = function() {
+        if(this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+
+            var response = JSON.parse(xhr.responseText);
+
+            var available = "Книга занята", dateLabel = "Дата возврата", giveTake = "Принять книгу";
+            document.getElementById("reader-block").style = "display: none";
+            if (response.available) {
+                available = "Кнгига свободна";
+                dateLabel = "Дата выдачи";
+                giveTake = "Выдать книгу";
+                document.getElementById("reader-block").style = "display: block";
+            }
+
+            console.log(response);
+            document.getElementById("update-result").innerHTML = "";
+            document.getElementById("update-form-id").innerHTML = response.id;
+            document.getElementById("update-form-state").innerHTML = available;
+            document.getElementById("update-form-title").value = response.title;
+            document.getElementById("update-form-author").value = response.author;
+            document.getElementById("update-form-year").value = response.year;
+            document.getElementById("date-label").innerHTML = dateLabel;
+            document.getElementById("give-take").innerHTML = giveTake;
+            document.getElementById("history").innerHTML = historyToHTML(response.history);
+            document.getElementById("update-form-image").src = response.image;
+            document.getElementById("overlay").style = "display: block";
+        } else {
+            document.getElementById("overlay").style = "display: none";
+        }
+    }
+    xhr.send(json);
+
 }
